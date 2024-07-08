dc5be216c18a7ade4313049a5f5221512f4182b0
Angotir <angotir@gmail.com>
2024-07-08 12:26:13 +0400

    comment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -12,6 +12,12 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
     
+    // Comment1
+
+    /*/comment 2
+     * comment 3
+    /*/
+
     void Start()
     {
         Time.timeScale = _timeScale;
