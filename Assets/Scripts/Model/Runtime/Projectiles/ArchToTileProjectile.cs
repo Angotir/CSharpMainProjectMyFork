2d7b853c0779a5ca5e3f6debdb75f9453a0a28b8
Angotir <angotir@gmail.com>
2024-07-11 11:59:41 +0400

    PeskovHomework2

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -30,11 +30,14 @@ namespace Model.Runtime.Projectiles
             // Insert you code here
             ///////////////////////////////////////
 
+            float maxHeighCoeff = 0.6f;
+            float maxHeight = totalDistance * maxHeighCoeff;
+            localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
 
             ///////////////////////////////////////
             // End of the code to insert
             ///////////////////////////////////////
-            
+
             Height = localHeight;
             if (time > StartTime + _timeToTarget)
                 Hit(_target);
